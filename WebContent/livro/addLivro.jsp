43e1123dc5af84b66a767596997d5f885fe9ffc5
unknown <14280090@W3070570.portoalegre.pucrsnet.br>
2016-06-01 19:08:55 -0300

    Upload de arquivo funcionando

--- WebContent/livro/addLivro.jsp
+++ WebContent/livro/addLivro.jsp
@@ -13,21 +13,17 @@
 		<jsp:include page="/template/msg.jsp"></jsp:include>
 
 
-		<form method="post" action="main?acao=cadastraLivro">
-			<div class="form-group">
-				<label  class="form-label"> ISBN Livro</label>
-				<input class="form-control" name="isbn" id="isbn">
-				<label class="form-label" > Titulo Livro</label>
-				<input class="form-control" name="titulo" id="titulo">
-				<hr>
-				<div class="text-center">
-					<input class="btn btn-danger" type="reset"
-						value="Cancelar">
-					<input class="btn btn-info" type="submit"
-						value="Cadastro">
-				</div>
-			</div>
+            
+        <form method="post" action='upload' enctype="multipart/form-data" >
+			<label class="form-label ages">Projeto: </label>
+
+			<label class="form-label ages">Arquivo:</label>
+			<input class="form-control" id="file" name="file" value="${param.file}" type="file" maxlength="120">
+			
+			<input class="btn btn-primary pull-right" type="submit"	value="Enviar">
 		</form>
+            
+
 	</div>
 </div>
 
--- src/br/ages/crud/servlet/FileUploadServlet.java
+++ src/br/ages/crud/servlet/FileUploadServlet.java
@@ -16,7 +16,98 @@ import org.apache.log4j.Logger;
 
 import br.ages.crud.model.Projeto;
 import br.ages.crud.util.Constantes;
+ 
+/**
+ * A Java servlet that handles file upload from client.
+ *
+ * @author www.codejava.net
+ *//*
+@WebServlet(name="/upload")
+public class FileUploadServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+     
+    // location to store file uploaded
+    private static final String UPLOAD_DIRECTORY = "c:\\uploadProjetos";
+ 
+    // upload settings
+    private static final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
+    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
+ 
 
+/**
+     * Upon receiving file upload submission, parses the request to read
+     * upload data and saves the file on disk.
+     *//*
+   
+       /
+protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        // checks if the request actually contains upload file
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            // if not, we stop here
+            PrintWriter writer = response.getWriter();
+            writer.println("Error: Form must has enctype=multipart/form-data.");
+            writer.flush();
+            return;
+        }
+ 
+        // configures upload settings
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // sets memory threshold - beyond which files are stored in disk
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        // sets temporary location to store files
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+ 
+        ServletFileUpload upload = new ServletFileUpload(factory);
+         
+        // sets maximum size of upload file
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+         
+        // sets maximum size of request (include file + form data)
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+ 
+        // constructs the directory path to store upload file
+        // this path is relative to application's directory
+        String uploadPath = getServletContext().getRealPath("")
+                + File.separator + UPLOAD_DIRECTORY;
+         
+        // creates the directory if it does not exist
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+ 
+        try {
+            // parses the request's content to extract file data
+            @SuppressWarnings("unchecked")
+            List<FileItem> formItems = upload.parseRequest(request);
+ 
+            if (formItems != null && formItems.size() > 0) {
+                // iterates over form's fields
+                for (FileItem item : formItems) {
+                    // processes only fields that are not form fields
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadPath + File.separator + fileName;
+                        File storeFile = new File(filePath);
+ 
+                        // saves the file on disk
+                        item.write(storeFile);
+                        request.setAttribute("message",
+                            "Upload has been done successfully!");
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            request.setAttribute("message",
+                    "There was an error: " + ex.getMessage());
+        }
+        // redirects client to message page
+        getServletContext().getRequestDispatcher("/addLivro.jsp").forward(
+                request, response);
+    }
+    */
 @WebServlet("/upload")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 15, maxFileSize = Constantes.PROJETO_ARQUIVO_MAX_BYTES, maxRequestSize = 1024 * 1024 * 15)
 public class FileUploadServlet extends HttpServlet {
@@ -30,10 +121,8 @@ public class FileUploadServlet extends HttpServlet {
 
 		try {
 			logger.debug("Iniciando o Upload");
-			Projeto projeto = (Projeto) request.getSession().getAttribute("projeto");
 
-			String appPath = projeto.getWorkspace();
-			String savePath = SAVE_DIR + File.separator + appPath ;
+			String savePath = SAVE_DIR + File.separator ;
 			File fileSaveDir = new File(savePath);
 
 			if (!fileSaveDir.exists())
@@ -45,8 +134,7 @@ public class FileUploadServlet extends HttpServlet {
 
 			request.setAttribute("msgSucesso", "Upload feito com sucesso!");
 
-			// request.setAttribute("acao", "listaProjetos");
-			getServletContext().getRequestDispatcher("/main?acao=listaProjetos").forward(request, response);
+			getServletContext().getRequestDispatcher("/main?acao=telaLivro").forward(request, response);
 			
 			logger.info("Executado o Upload em: " + savePath + " - " + fileName );
 		} catch (Exception e) {
@@ -66,4 +154,11 @@ public class FileUploadServlet extends HttpServlet {
 		return "";
 	}
 	
-	}
+	
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	super.doPost(req, resp);
+    }
+}
