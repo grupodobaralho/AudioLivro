d50a99caaa374a1ded03639c5fe31888cbe4edce
Vinicius Jaggi <vinicius.jaggi@acad.pucrs.br>
2016-08-31 19:06:25 -0300

    trabalho na classe de testes do BlocoBO

--- test/br/ages/audio/bo/TestBlocoBO.java
+++ test/br/ages/audio/bo/TestBlocoBO.java
@@ -1,5 +1,38 @@
 package br.ages.audio.bo;
 
-public class TestBlocoBO {
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import br.ages.crud.dao.BlocoDAO;
+import br.ages.crud.dao.CapituloBlocoDAO;
+import br.ages.crud.model.Bloco;
 
+public class TestBlocoBO {
+	
+	private BlocoBO blocoBO;
+	private Bloco blocos;
+	
+	 @Mock
+	 BlocoDAO bloco;
+	 CapituloBlocoDAO capitulo;
+	 
+	 @Before
+	 public void TestBlocoBO(){
+		blocoBO = new BlocoBO(); 
+		blocos = new Bloco();
+		 
+	      
+	 }
+	 
+	 
+	 @Test
+	 public void testCadastraBloco(){
+	 
+
+	 }
+	 
+	 @Test
+	 public void testAlteraCaminho(){
+		 
+ 	}
 }
