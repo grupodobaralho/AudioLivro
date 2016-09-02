433167be09b720c201f6772e6595e5706e33555a
Vinicius Jaggi <vinicius.jaggi@acad.pucrs.br>
2016-09-02 15:11:59 -0300

    criação do teste cadastraBloco

--- test/br/ages/audio/bo/TestBlocoBO.java
+++ test/br/ages/audio/bo/TestBlocoBO.java
@@ -1,34 +1,46 @@
 package br.ages.audio.bo;
 
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import br.ages.crud.dao.BlocoDAO;
 import br.ages.crud.dao.CapituloBlocoDAO;
+import br.ages.crud.exception.NegocioException;
+import br.ages.crud.exception.PersistenciaException;
 import br.ages.crud.model.Bloco;
-
+import br.ages.crud.model.Status;
+@RunWith(MockitoJUnitRunner.class)
 public class TestBlocoBO {
 	
 	private BlocoBO blocoBO;
-	private Bloco blocos;
+	private Bloco bloco;
 	
 	 @Mock
-	 BlocoDAO bloco;
-	 CapituloBlocoDAO capitulo;
+	 BlocoDAO blocoDAO;
+	 CapituloBlocoDAO capituloDAO;
 	 
 	 @Before
-	 public void TestBlocoBO(){
-		blocoBO = new BlocoBO(); 
-		blocos = new Bloco();
+	 public void TestBlocoBOTest(){
 		 
-	      
+		 bloco = new Bloco("conteudo","audio",Status.APROVADO);
+		 bloco.setId_bloco(20);
 	 }
 	 
 	 
 	 @Test
-	 public void testCadastraBloco(){
+	 public void testCadastraBloco() throws PersistenciaException, NegocioException, SQLException{
 	 
-
+		 Mockito.when(blocoDAO.cadastraBloco(bloco)).thenReturn(20);   
+		 int condition = blocoBO.cadastraBloco(bloco, 30);
+		 assertEquals(20,condition);
 	 }
 	 
 	 @Test
