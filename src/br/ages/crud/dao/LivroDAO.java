3d4b7e33fc34abca0a51b73c70909b4a1660223f
16104292 <16104292@W3070646.portoalegre.pucrsnet.br>
2016-11-07 18:56:35 -0200

    Métodos de exclusão de livro.

--- src/br/ages/crud/dao/LivroDAO.java
+++ src/br/ages/crud/dao/LivroDAO.java
@@ -20,29 +20,29 @@ import br.ages.crud.util.ConexaoUtil;
 import br.ages.crud.util.MensagemContantes;
 
 public class LivroDAO {
-	
+
 	private ArrayList<Livro> listarLivros;
-	
-	public LivroDAO(){
+
+	public LivroDAO() {
 		listarLivros = new ArrayList<>();
 	}
 
 	public int cadastraLivro(Livro livro) throws PersistenciaException, SQLException {
-	
+
 		Connection conexao = null;
 		try {
 			Integer idLivro = null;
 			conexao = ConexaoUtil.getConexao();
-			
+
 			StringBuilder sql = new StringBuilder();
 			sql.append("insert into tb_livro (Titulo, ISBN, Autores, Data_criacao, Data_alteracao, Status)");
 			sql.append("values (?,?,?,?,?,?)");
-			
+
 			// converte a data para data Juliana, data que o banco reconhece
 			java.util.Date utilDate = new java.util.Date();
 			java.sql.Date dataCadastro = new java.sql.Date(utilDate.getTime());
-			
-			//cadastra o livro e gera id
+
+			// cadastra o livro e gera id
 			PreparedStatement statement = conexao.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
 			statement.setString(1, livro.getTitulo());
 			statement.setString(2, livro.getISBN());
@@ -50,30 +50,31 @@ public class LivroDAO {
 			statement.setDate(4, dataCadastro);
 			statement.setDate(5, dataCadastro);
 			statement.setString(6, StatusLivroEnum.INCOMPLETO.toString());
-			
+
 			statement.executeUpdate();
-			
+
 			ResultSet resultset = statement.getGeneratedKeys();
-			if(resultset.first()){
+			if (resultset.first()) {
 				idLivro = resultset.getInt(1);
 			}
 			return idLivro;
 		} catch (ClassNotFoundException | SQLException e) {
-			throw new PersistenciaException(MensagemContantes.MSG_ERR_LIVRO_JA_EXISTENTE.replace("?", livro.getTitulo()));
+			throw new PersistenciaException(
+					MensagemContantes.MSG_ERR_LIVRO_JA_EXISTENTE.replace("?", livro.getTitulo()));
 		} finally {
 			conexao.close();
 		}
 	}
-	
+
 	public boolean atualizarLivro(Livro livro) throws PersistenciaException, SQLException {
 		boolean returnUpdate = false;
 		Connection conexao = null;
 		try {
 			conexao = ConexaoUtil.getConexao();
-			
+
 			java.util.Date utilDate = new java.util.Date();
 			java.sql.Date sysdate = new java.sql.Date(utilDate.getTime());
-			
+
 			StringBuilder sql = new StringBuilder();
 			sql.append("update tb_livro set ");
 			sql.append("Titulo = ?, ");
@@ -101,52 +102,54 @@ public class LivroDAO {
 		}
 		return returnUpdate;
 	}
-	public boolean excluirLivro(Livro livro) throws PersistenciaException, SQLException{
+
+	public boolean excluirLivro(int idLivro) throws PersistenciaException, SQLException {
 		boolean returnUpdate = false;
 		Connection conexao = null;
-		try{
+		try {
 			conexao = ConexaoUtil.getConexao();
 
 			java.util.Date utilDate = new java.util.Date();
 			java.sql.Date sysdate = new java.sql.Date(utilDate.getTime());
-			
+
 			StringBuilder sql = new StringBuilder();
 			sql.append("update tb_Livro set ");
 			sql.append("Data_alteracao = ? , ");
 			sql.append("status = ? ");
 			sql.append("where id_livro = ?");
-			
+
 			PreparedStatement statement = conexao.prepareStatement(sql.toString());
 			statement.setDate(1, sysdate);
 			statement.setString(2, StatusLivroEnum.EXCLUIDO.toString());
-			statement.setInt(3, livro.getIdLivro());
+			statement.setInt(3, idLivro);
 			statement.execute();
 			returnUpdate = true;
-			
-		}catch (ClassNotFoundException | SQLException e){
+
+		} catch (ClassNotFoundException | SQLException e) {
 			throw new PersistenciaException(e);
-		}finally{
+		} finally {
 			conexao.close();
-		
-	}
+
+		}
 		return returnUpdate;
 	}
+
 	public Livro buscarLivro(int idLivro) throws PersistenciaException, SQLException {
 		Livro livro = null;
-		
+
 		Connection conexao = null;
 		try {
 			conexao = ConexaoUtil.getConexao();
-			
+
 			StringBuilder sql = new StringBuilder();
 			sql.append("select id_livro, isbn, titulo, autores from tb_livro ");
 			sql.append("where id_livro = ?");
-			
+
 			PreparedStatement statement = conexao.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
 			statement.setInt(1, idLivro);
-			
+
 			ResultSet resultset = statement.executeQuery();
-			if(resultset.next()){
+			if (resultset.next()) {
 				livro = new Livro();
 				livro.setIdLivro(resultset.getInt("ID_LIVRO"));
 				livro.setISBN(resultset.getString("ISBN"));
@@ -161,7 +164,7 @@ public class LivroDAO {
 			conexao.close();
 		}
 	}
-	
+
 	public List<Livro> listarLivros() throws PersistenciaException, SQLException, NegocioException {
 		Connection conexao = null;
 		// tentativa de readaptação do listarUsuarios()
@@ -181,13 +184,12 @@ public class LivroDAO {
 				livro.setTitulo(resultset.getString("TITULO"));
 				livro.setAutores(resultset.getString("AUTORES"));
 				livro.setStatus(StatusLivroEnum.valueOf(resultset.getString("STATUS")));
-				
+
 				CapituloBO capituloBO = new CapituloBO();
 				ArrayList<Capitulo> capitulosLivro = capituloBO.buscarCapitulosDoLivro(livro);
-				
+
 				livro.setCapitulos(capitulosLivro);
-				
-				
+
 				listarLivros.add(livro);
 			}
 
@@ -200,40 +202,39 @@ public class LivroDAO {
 	}
 
 	public List<Livro> listarLivros(StatusLivroEnum status) throws PersistenciaException, SQLException {
-			Connection conexao = null;
-			
-			try {
-				conexao = ConexaoUtil.getConexao();
+		Connection conexao = null;
 
-				StringBuilder sql = new StringBuilder();
-				sql.append("select ID_LIVRO, ISBN, TITULO, AUTORES ");
-				sql.append("from audio_e.tb_livro");
-				sql.append("where STATUS =?");
-				
-				PreparedStatement statement = conexao.prepareStatement(sql.toString());
-				
-				statement.setString(1, status.toString());
-				
-				ResultSet resultset = statement.executeQuery();
-				
-				while (resultset.next()) {
-					Livro livro = new Livro();
-					livro.setIdLivro(resultset.getInt("ID_LIVRO"));
-					livro.setISBN(resultset.getString("ISBN"));
-					livro.setTitulo(resultset.getString("TITULO"));
-					livro.setAutores(resultset.getString("AUTORES"));
-					
-					listarLivros.add(livro);
-				}
+		try {
+			conexao = ConexaoUtil.getConexao();
 
-			} catch (ClassNotFoundException | SQLException e) {
-				throw new PersistenciaException(e);
-			} finally {
-				conexao.close();
+			StringBuilder sql = new StringBuilder();
+			sql.append("select ID_LIVRO, ISBN, TITULO, AUTORES ");
+			sql.append("from audio_e.tb_livro");
+			sql.append("where STATUS =?");
+
+			PreparedStatement statement = conexao.prepareStatement(sql.toString());
+
+			statement.setString(1, status.toString());
+
+			ResultSet resultset = statement.executeQuery();
+
+			while (resultset.next()) {
+				Livro livro = new Livro();
+				livro.setIdLivro(resultset.getInt("ID_LIVRO"));
+				livro.setISBN(resultset.getString("ISBN"));
+				livro.setTitulo(resultset.getString("TITULO"));
+				livro.setAutores(resultset.getString("AUTORES"));
+
+				listarLivros.add(livro);
 			}
-			return listarLivros;
 
-		
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new PersistenciaException(e);
+		} finally {
+			conexao.close();
+		}
+		return listarLivros;
+
 	}
-	
+
 }
